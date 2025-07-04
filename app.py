7080a8b8772a147c979f3200d642e8a66ba4032d
Tejas Agarwal <66433176+Zeusius1407@users.noreply.github.com>
2025-07-04 15:49:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,18 @@ import os
 
 st.title("Before & After Style Transfer")
 
-# Directory paths
 content_dir = "images"
 output_dir = "outputs"
 style_dir = "styles"
 
-# List of available images (ensure names match)
 image_files = [f for f in os.listdir(content_dir) if f.endswith(('.jpg', '.png', 'jpeg'))]
 style_files = [s for s in os.listdir(style_dir) if s.endswith(('.jpg', '.png', 'jpeg'))]
-# Select image
+
 selected_image = st.selectbox("Choose an image to view:", image_files)
 selected_style = st.selectbox("Choose the style:", style_files)
 save_style = selected_style.split('.')[0]
 selected_output = f'{save_style}_{selected_image}'
-# Load before and after
+
 before = Image.open(os.path.join(content_dir, selected_image))
 after = Image.open(os.path.join(output_dir, selected_output))
 
